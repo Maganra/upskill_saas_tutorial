45e074e8f52547a8c4fc23b3a78b48db87347f0f
Maganra <brennan.l.olson@gmail.com>
2022-04-06 17:45:20 +0000

    mailer file changes

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,6 @@
 class ContactMailer < ActionMailer::Base
     default to: 'brennan.l.olson@gmail.com'
+    #default to: 'Brennan.Olson@drillingtools.com'
     
     def contact_email(name, email, body)
         @name = name
