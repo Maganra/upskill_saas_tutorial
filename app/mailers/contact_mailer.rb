2982eabfeb48f2734d6e286a29f2902c734b8240
Maganra <73654148+Maganra@users.noreply.github.com>
2022-04-06 16:27:37 -0500

    smtp setting in contact mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,12 +1,13 @@
 class ContactMailer < ActionMailer::Base
     default to: 'brennan.l.olson@gmail.com'
     #default to: 'Brennan.Olson@drillingtools.com'
-    
+
     def contact_email(name, email, body)
+        delivery_method = :smtp
         @name = name
         @email = email
         @body = body
-        
+
         mail(from: email, subject: 'Contact Form Message')
     end
 end
